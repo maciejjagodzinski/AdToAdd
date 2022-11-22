7cf2209bd0b7ad9415ce45aeb43f42c098b09b61
Maciej Jagodzinski <maciejjagodzinskizip@gmail.com>
2022-11-22 14:03:07 +0100

    Initialize project

---
+++ android/app/src/main/kotlin/com/example/sovotest/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sovotest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sovo test app',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'To do'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: Text(title),
+    ));
+  }
+}
